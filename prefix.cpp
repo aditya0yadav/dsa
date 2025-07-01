553eef182c5a76a199b0b72ab2d88bf1dd24f532
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2025-07-01 06:49:39 +0000

    optimized code

--- prefix.cpp
+++ prefix.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main() {
-	int n ;
-	cin >> n ;
-	vector <int> vec(n) ;
-	for(auto &it : v)l cin >> it 
-	int q ;
-	cin >> q ;
-	while (q--){
-	    int l,r ;
-	    cin >> l >> r ;
-	    int s = 0 ;
-	    for (int i = l ; i<= r, i++){
-	        s += vec[i]
-	    }
-	    cout << s ;
-	}
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &it : vec) cin >> it;
 
+    vector<int> pre(n);
+    pre[0] = vec[0];
+    for (int i = 1; i < n; i++) {
+        pre[i] = pre[i - 1] + vec[i];
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << pre[r] - (l == 0 ? 0 : pre[l - 1]) << endl;
+    }
 }
