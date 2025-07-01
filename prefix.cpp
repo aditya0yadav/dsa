7a01a00ebfdf4ee013f3650e981711e8a6ef95b8
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2025-07-01 06:29:26 +0000

    #1 commit prefix sum

---
+++ prefix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n ;
+	cin >> n ;
+	vector <int> vec(n) ;
+	for(auto &it : v)l cin >> it 
+	int q ;
+	cin >> q ;
+	while (q--){
+	    int l,r ;
+	    cin >> l >> r ;
+	    int s = 0 ;
+	    for (int i = l ; i<= r, i++){
+	        s += vec[i]
+	    }
+	    cout << s ;
+	}
+
+}
